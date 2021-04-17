97e592c9a064c0e2c4ef1ee2f6574f78eebc2f2f
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2021-04-17 10:06:55 +0700

    Listen from all addresses

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(port)
+    await fastify.listen(port, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
