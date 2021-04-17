9f523c2de8e87281e89873595b53d6f697b3ac4f
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2021-04-17 09:41:58 +0700

    Rewrite in async form

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')()
-const fastifyStatic = require('fastify-static');
-const port = process.env.PORT;
+const fastify = require('fastify')({ logger: true })
+const fastifyStatic = require('fastify-static')
+const port = process.env.PORT
 
 fastify.register(fastifyStatic, require('./config/static').public)
 
-fastify.get('/', function (req, reply) {
+// Declare a route
+fastify.get('/', async (request, reply) => {
   return reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
 })
 
 // Run the server!
-fastify.listen(port, (err) => {
-  if (err) {
+const start = async () => {
+  try {
+    await fastify.listen(port)
+  } catch (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-})
+}
+start()
