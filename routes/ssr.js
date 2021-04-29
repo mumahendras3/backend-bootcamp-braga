c045f3daa48b354a68c65416ccffbb5a773b1a59
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2021-04-29 14:03:42 +0700

    Refactored portfolio page and generate filters dynamically from fetched database data

--- routes/ssr.js
+++ routes/ssr.js
@@ -1,24 +1,27 @@
-const data = require("../data/portfolio.json");
-
 async function routes(fastify, options) {
   // Portfolio page
   fastify.get("/portfolio.html", async (request, reply) => {
     const { rows } = await fastify.pg.query(
       "SELECT filter, img_src, title, summary, gallery_href, gallery_title FROM portfolio_items"
     );
-    reply.view("/public/portfolio.ejs", {
-      ...data,
-      items: rows.map((row) => {
-        return {
-          filter: row.filter,
-          imgSrc: row.img_src,
-          title: row.title,
-          summary: row.summary,
-          galleryHref: row.gallery_href,
-          galleryTitle: row.gallery_title,
-        };
-      }),
+    // Create an array containing all available filters
+    let filters = [];
+    rows.forEach((row) => {
+      filters.push(row.filter);
     });
+    // Remove duplicate filters
+    const uniqueFilters = filters.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+    // Generate a suitable data for use by ejs
+    const data = { filters: [], items: rows };
+    uniqueFilters.forEach((filter) => {
+      data["filters"].push({
+        "filter-class": `.${filter}`,
+        "filter-text": filter.split("-")[1],
+      });
+    });
+    reply.view("/public/portfolio.ejs", data);
   });
 
   // Resume page
